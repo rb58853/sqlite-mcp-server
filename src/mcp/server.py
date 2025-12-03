df48b1332869405c55713a1e2c6c5a31696d9b4f
rb58853 <rb58853@gmail.com>
2025-12-03 11:04:35 -0500

    Update prompts && Database

--- src/mcp/server.py
+++ src/mcp/server.py
@@ -1,7 +1,10 @@
 from mcp.server.fastmcp import FastMCP
 from . import Tools, Prompts, Resources
 
-sqlite_mcp_server: FastMCP = FastMCP(name="sqlite_mcp_server", stateless_http=True)
+sqlite_mcp_server: FastMCP = FastMCP(
+    name="sqlite_mcp_server",
+    stateless_http=False,
+)
 
 Tools().registry(sqlite_mcp_server)
 Prompts().registry(sqlite_mcp_server)
